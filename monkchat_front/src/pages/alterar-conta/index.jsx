1643346f0f388aced4257844dfe21631b8bf63ad
Fabriciobr5975 <araujofabricio71@gmail.com>
2025-04-25 20:53:12 -0300

    Tela de alteração

--- monkchat_front/src/pages/alterar-conta/index.jsx
+++ monkchat_front/src/pages/alterar-conta/index.jsx
@@ -1,8 +1,32 @@
 import "./index.scss";
 import BarraLogo from "../../components/barra-logo";
-import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 export default function AlterarConta() {
+  const location = useLocation();
+  const { id, email, senha } = location.state || {};
+  const [novaSenha, setNovaSenha] = useState(senha);
+  const [nick, setNick] = useState("");
+
+  async function alterarUsuario() {
+    const body = {
+      nm_usuario: nick,
+      ds_email: email,
+      ds_senha: novaSenha,
+    };
+
+    let resp = await axios.put(`http://localhost:5001/usuario/${id}`, body);
+
+    if (resp.data.resposta >= 1) {
+      alert(`O usuário foi alterado com sucesso`);
+    
+    } else {
+      alert(`O usuário não foi alterado`)
+    }
+  }
+
   return (
     <div className="alterar-app">
       <BarraLogo />
@@ -11,25 +35,37 @@ export default function AlterarConta() {
         <label className="titulo">Alterar conta</label>
 
         <label className="info">Email</label>
-        <input type="email" placeholder="Insira seu e-mail" className="input" />
+        <input
+          type="email"
+          value={email}
+          placeholder="Insira seu e-mail"
+          className="input"
+          readOnly
+        />
 
         <label className="info">Senha</label>
         <input
           type="password"
+          value={novaSenha}
+          onChange={(e) => setNovaSenha(e.target.value)}
           placeholder="Insira seu e-mail"
           className="input"
         />
 
-        <label className="info">Nick</label>
+        <label className="info" value={nick}>
+          Nick
+        </label>
         <input
           type="text"
           placeholder="Insira seu nickname"
           className="input"
+          onChange={(e) => setNick(e.target.value)}
         />
 
-        <button className="btn-alterar">Alterar</button>
+        <button className="btn-alterar" onClick={alterarUsuario}>
+          Alterar
+        </button>
       </div>
     </div>
   );
 }
-
