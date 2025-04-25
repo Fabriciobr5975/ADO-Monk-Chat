ef5753c9e24fb1db07a96d4f94df507607f0bdbd
gabriel <garoschel11@gmail.com>
2025-04-25 17:29:56 -0300

    front com axios

--- monkchat_front/src/pages/registrar/index.jsx
+++ monkchat_front/src/pages/registrar/index.jsx
@@ -1,8 +1,14 @@
 import "./index.scss";
 import BarraLogo from "../../components/barra-logo";
+import axios from "axios";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [nick, setNick] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
   async function registrar() {}
 
   return (
@@ -17,16 +23,23 @@ function App() {
           type="text"
           placeholder="Insira seu nickname"
           className="input"
+          onChange={(e) => setNick(e.target.value)}
         />
 
         <label className="info">Email</label>
-        <input type="email" placeholder="Insira seu e-mail" className="input" />
+        <input
+          type="email"
+          placeholder="Insira seu e-mail"
+          className="input"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label className="info">Senha</label>
         <input
           type="password"
           placeholder="Insira seu e-mail"
           className="input"
+          onChange={(e) => setSenha(e.target.value)}
         />
 
         <button className="btn-criar">Criar</button>
