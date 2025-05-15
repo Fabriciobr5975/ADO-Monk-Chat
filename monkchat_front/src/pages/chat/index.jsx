8051ee72a86c45d880137bfcf32ad930d34998d0
MARCUS VINICIUS PEREIRA ROCHA <marcus.vprocha1@senacsp.edu.br>
2025-05-15 11:31:36 -0300

    adicionando usestate

--- monkchat_front/src/pages/chat/index.jsx
+++ monkchat_front/src/pages/chat/index.jsx
@@ -4,10 +4,96 @@ import line from "../../assets/images/Line 1.png"
 import atualizar from "../../assets/images/Botao Atualizar.png"
 import edit from "../../assets/images/edit.svg"
 import remove from "../../assets/images/remove.svg"
+import axios from "axios";
+import { useState } from "react";
+
 
 
 
 export default function Chat() {
+    const [nick, setNick] = useState('');
+    const [sala, setSala] = useState('');
+    const [dest, setDest] = useState('');
+    const [texto, setTexto] = useState('');
+    const [mensagem, setMensagem] = useState([
+        {
+            ID_USUARIO_ENVIO:"",
+            ID_USUARIO_PARA:"",
+            ID_SALA:"",
+            DS_MENSAGEM:"",
+            DT_MENSAGEM:getCurrentDate()
+        }
+    ]);
+    function getCurrentDate() {
+    var nowDate = new Date();
+    var month =
+      (nowDate.getMonth() + 1).toString().length === 1
+        ? "0" + (nowDate.getMonth() + 1)
+        : nowDate.getMonth() + 1;
+
+    var day =
+      nowDate.getDate().toString().length === 1
+        ? "0" + nowDate.getDate()
+        : +nowDate.getDate();
+
+    var hour =
+      nowDate.getHours().toString().length === 1
+        ? "0" + nowDate.getHours()
+        : +nowDate.getHours();
+
+    var minute =
+      nowDate.getMinutes().toString().length === 1
+        ? "0" + nowDate.getMinutes()
+        : +nowDate.getMinutes();
+
+    var seconds =
+      nowDate.getSeconds().toString().length === 1
+        ? "0" + nowDate.getSeconds()
+        : +nowDate.getSeconds();
+
+    return (
+      month +
+      "-" +
+      day +
+      " " +
+      hour +
+      ":" +
+      minute +
+      ":" +
+      seconds
+    );
+  }
+
+
+    async function Deletar() {
+        
+    }
+    async function Listar() {
+
+    }
+    async function Enviar() {
+
+    }
+    async function Alterar() {
+
+    }
+    async function Criar() {
+        let body = {
+            ID_USUARIO: "",
+            NM_SALA: ""
+        }
+        const resp = await axios.post("/sala", body);
+
+    }
+    async function Entrar() {
+        let body = [
+            
+        ]
+        const resp = await axios.post("/participante");
+    }
+
+
+
     return (
         <main>
             <header>
@@ -26,23 +112,24 @@ export default function Chat() {
                 <div className="container">
                     <div className="input">
                         <div className="dados">
-                            <div className="insercao-dados"> Sala: <input type="text" /></div>
-                            <div className="insercao-dados"> Nick: <input type="text" /> </div>
-                            <div className="insercao-dados"> Para: <input type="text" /> </div>
+                            <div className="insercao-dados"> Sala: <input type="text" onChange={(e)=> setSala(e.target.value)}/></div>
+                            <div className="insercao-dados"> Nick: <input type="text" onChange={(e)=> setNick(e.target.value)}/> </div>
+                            <div className="insercao-dados"> Para: <input type="text" onChange={(e)=> setDest(e.target.value)}/> </div>
                             <div className="insercao-dados">
-                                <label ></label><div><input type="button" value="Criar" /> <input type="button" value="Entrar" /></div>
+                                <label ></label><div><input type="button" value="Criar" onClick={Criar} />
+                                    <input type="button" value="Entrar" onClick={Entrar} /></div>
                             </div>
 
                         </div>
                         <div className="mensagem">
                             Mensagem:
-                            <textarea name="areatexto" id=""></textarea>
-                            <input type="button" value="Entrar" />
+                            <textarea name="areatexto" id="" onChange={(e)=> setTexto(e.target.value)}></textarea>
+                            <input type="button" value="Enviar" onClick={Enviar} />
                         </div>
                     </div>
                     <div className="chat">
                         <div className="atualizar">
-                            <div><img src={atualizar} alt="" /></div>
+                            <div><img src={atualizar} alt="" onClick={Listar} /></div>
                         </div>
                         <div className="areadochat">
                             <div className="mensagemnochat">
@@ -52,9 +139,9 @@ export default function Chat() {
                             </div>
                             <div className="mensagemnochat">
                                 <div>(15:03:25) <strong>{"Brunex"}</strong> fala para <strong>{"Todos"}</strong>: Opa, tudo bem ai?</div>
-                                <div className="edit-remove"> 
-                                    <img src={edit} alt="" />
-                                    <img src={remove} alt="" />
+                                <div className="edit-remove">
+                                    <img src={edit} alt="" onClick={Alterar} />
+                                    <img src={remove} alt="" onClick={Deletar} />
                                 </div>
                             </div >
                             <div className="mensagemnochat">
@@ -64,8 +151,8 @@ export default function Chat() {
                                 <div>
                                     (15:03:25) <strong>{"Brunex"}</strong> fala para <strong>{"Osvaldo"}</strong>: To com fomeeeee
                                 </div>
-                                
-                                <div className="edit-remove"> 
+
+                                <div className="edit-remove">
                                     <img src={edit} alt="" />
                                     <img src={remove} alt="" />
                                 </div>
